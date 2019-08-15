1ee0625987773a7c2a849622f8af6915ac5608cb
Viktor Smakhtin <viktor.smakhtin@sharksw.com>
2019-08-15 12:43:48 +0300

    Trunk indenty layout

--- src/components/TrunkGroupPage/Tabs/TrunkIndenty/index.js
+++ src/components/TrunkGroupPage/Tabs/TrunkIndenty/index.js
@@ -8,8 +8,12 @@ import Col from "react-bootstrap/lib/Col";
 import FormControl from "react-bootstrap/lib/FormControl";
 import InputGroup from "react-bootstrap/lib/InputGroup";
 import Button from "react-bootstrap/lib/Button";
+import Panel from "react-bootstrap/lib/Panel";
 
-import { fetchGetGroupById } from "../../../../store/actions";
+import {
+  fetchGetGroupById,
+  postAddKeyToApplication
+} from "../../../../store/actions";
 import Loading from "../../../../common/Loading";
 
 export class TrunkIndenty extends Component {
@@ -59,131 +63,148 @@ export class TrunkIndenty extends Component {
     return (
       <React.Fragment>
         <Row className={"margin-top-1"}>
-          <Col md={12} className={"flex align-items-center"}>
-            <div className={"margin-right-1 flex flex-basis-16"}>
-              {`tgrp@${this.props.group.defaultDomain}`}
-            </div>
-            <InputGroup>
-              <FormControl
-                type="text"
-                value={this.state.trunkGroupIdentity}
-                onChange={e =>
-                  this.setState({ trunkGroupIdentity: e.target.value })
-                }
-              />
-              <InputGroup.Addon>{`@${
-                this.props.group.defaultDomain
-              }`}</InputGroup.Addon>
-            </InputGroup>
+          <Col md={6}>
+            <Panel>
+              <Panel.Heading>
+                <div className={"header"}>Trunkgroup/trunk-context</div>
+                <div>Settings related to trunkgroup/trunk-context</div>
+              </Panel.Heading>
+              <Panel.Body>
+                <div className={"flex align-items-center"}>
+                  <div className={"margin-right-1 flex flex-basis-16"}>
+                    {`tgrp@${this.props.group.defaultDomain}`}
+                  </div>
+                  <InputGroup>
+                    <FormControl
+                      type="text"
+                      value={this.state.trunkGroupIdentity}
+                      onChange={e =>
+                        this.setState({ trunkGroupIdentity: e.target.value })
+                      }
+                    />
+                    <InputGroup.Addon>{`@${
+                      this.props.group.defaultDomain
+                    }`}</InputGroup.Addon>
+                  </InputGroup>
+                </div>
+                <div>
+                  <Checkbox
+                    checked={this.state.allowTerminationToTrunkGroupIdentity}
+                    onChange={e => {
+                      this.setState({
+                        allowTerminationToTrunkGroupIdentity: e.target.checked
+                      });
+                    }}
+                  >
+                    {`Allow calls directly to tgrp@${
+                      this.props.group.defaultDomain
+                    }`}
+                  </Checkbox>
+                </div>
+                <div>
+                  <Checkbox
+                    checked={this.state.includeTrunkGroupIdentity}
+                    onChange={e => {
+                      this.setState({
+                        includeTrunkGroupIdentity: e.target.checked
+                      });
+                    }}
+                  >
+                    {`Include tgrp@${
+                      this.props.group.defaultDomain
+                    } in R-URI to PBX`}
+                  </Checkbox>
+                </div>
+                <div>
+                  <Checkbox
+                    checked={
+                      this.state.includeTrunkGroupIdentityForNetworkCalls
+                    }
+                    onChange={e => {
+                      this.setState({
+                        includeTrunkGroupIdentityForNetworkCalls:
+                          e.target.checked
+                      });
+                    }}
+                  >
+                    {`Include tgrp@${
+                      this.props.group.defaultDomain
+                    } in R-URI for external calls`}
+                  </Checkbox>
+                </div>
+              </Panel.Body>
+            </Panel>
           </Col>
-        </Row>
-        <Row className={"margin-top-1"}>
-          <Col md={12} className={"flex align-items-center"}>
-            <div className={"margin-right-1 flex flex-basis-16"}>OTG/DTG</div>
-            <div>
-              <FormControl
-                type="text"
-                value={this.state.otgDtgIdentity}
-                onChange={e => {
-                  this.setState({
-                    otgDtgIdentity: e.target.value
-                  });
-                }}
-              />
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12}>
-            <Checkbox
-              checked={this.state.allowTerminationToTrunkGroupIdentity}
-              onChange={e => {
-                this.setState({
-                  allowTerminationToTrunkGroupIdentity: e.target.checked
-                });
-              }}
-            >
-              {`Allow calls directly to tgrp@${this.props.group.defaultDomain}`}
-            </Checkbox>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12}>
-            <Checkbox
-              checked={this.state.allowTerminationToDtgIdentity}
-              onChange={e => {
-                this.setState({
-                  allowTerminationToDtgIdentity: e.target.checked
-                });
-              }}
-            >
-              Allow calls directly to the dtg
-            </Checkbox>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12}>
-            <Checkbox
-              checked={this.state.includeTrunkGroupIdentity}
-              onChange={e => {
-                this.setState({
-                  includeTrunkGroupIdentity: e.target.checked
-                });
-              }}
-            >
-              {`Include tgrp@${this.props.group.defaultDomain} in R-URI to PBX`}
-            </Checkbox>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12}>
-            <Checkbox
-              checked={this.state.includeDtgIdentity}
-              onChange={e => {
-                this.setState({
-                  includeDtgIdentity: e.target.checked
-                });
-              }}
-            >
-              Include DTG in R-URI to PBX
-            </Checkbox>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12}>
-            <Checkbox
-              checked={this.state.includeTrunkGroupIdentityForNetworkCalls}
-              onChange={e => {
-                this.setState({
-                  includeTrunkGroupIdentityForNetworkCalls: e.target.checked
-                });
-              }}
-            >
-              {`Include tgrp@${
-                this.props.group.defaultDomain
-              } in R-URI for external calls`}
-            </Checkbox>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12}>
-            <Checkbox
-              checked={this.state.includeOtgIdentityForNetworkCalls}
-              onChange={e => {
-                this.setState({
-                  includeOtgIdentityForNetworkCalls: e.target.checked
-                });
-              }}
-            >
-              Include OTG in R-URI for external calls
-            </Checkbox>
+          <Col md={6}>
+            <Panel>
+              <Panel.Heading>
+                <div className={"header"}>OTG/DTG</div>
+                <div>
+                  Settings related to Origination & destination trunkgroup
+                </div>
+              </Panel.Heading>
+              <Panel.Body>
+                <div className={"flex align-items-center"}>
+                  <div className={"margin-right-1 flex flex-basis-16"}>
+                    OTG/DTG
+                  </div>
+                  <div>
+                    <FormControl
+                      type="text"
+                      value={this.state.otgDtgIdentity}
+                      onChange={e => {
+                        this.setState({
+                          otgDtgIdentity: e.target.value
+                        });
+                      }}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <Checkbox
+                    checked={this.state.allowTerminationToDtgIdentity}
+                    onChange={e => {
+                      this.setState({
+                        allowTerminationToDtgIdentity: e.target.checked
+                      });
+                    }}
+                  >
+                    Allow calls directly to the dtg
+                  </Checkbox>
+                </div>
+                <div>
+                  <Checkbox
+                    checked={this.state.includeDtgIdentity}
+                    onChange={e => {
+                      this.setState({
+                        includeDtgIdentity: e.target.checked
+                      });
+                    }}
+                  >
+                    Include DTG in R-URI to PBX
+                  </Checkbox>
+                </div>
+                <div>
+                  <Checkbox
+                    checked={this.state.includeOtgIdentityForNetworkCalls}
+                    onChange={e => {
+                      this.setState({
+                        includeOtgIdentityForNetworkCalls: e.target.checked
+                      });
+                    }}
+                  >
+                    Include OTG in R-URI for external calls
+                  </Checkbox>
+                </div>
+              </Panel.Body>
+            </Panel>
           </Col>
         </Row>
         <Row className={"margin-top-1"}>
           <Col md={12}>
             <div className="button-row">
               <div className="pull-right">
-                <Button className={"btn-primary"}>&nbsp; Update</Button>
+                <Button className={"btn-primary"}>Update</Button>
               </div>
             </div>
           </Col>
