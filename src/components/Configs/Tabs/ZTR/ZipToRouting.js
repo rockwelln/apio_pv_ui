dc1c859c77750956b2a40b153af84cec44e8399a
Viktor Smakhtin <viktor.smakhtin@sharksw.com>
2020-04-27 14:28:09 +0300

    109 Jimmy - [PRAoSIP: Emergency routing] Unable to upload file

--- src/components/Configs/Tabs/ZTR/ZipToRouting.js
+++ src/components/Configs/Tabs/ZTR/ZipToRouting.js
@@ -227,7 +227,7 @@ export class ZipToRouting extends Component {
     for (let i = withHeaders ? 1 : 0; i < lines.length; i++) {
       let obj = {};
       let currentline = lines[i].split(";");
-      if (currentline.length > 2) {
+      if (currentline.length !== 2) {
         let errorHeader = "Count columns error";
         let errorText = "You must have two columns in the file";
 
@@ -239,7 +239,9 @@ export class ZipToRouting extends Component {
         });
         return [];
       }
+      console.log(currentline);
       for (let j = 0; j < headers.length; j++) {
+        console.log(currentline[j]);
         if (currentline[j].length && currentline[j].length > 255) {
           let errorHeader = "Value length error";
           let errorText = "Your max length of value must be not more 255";
